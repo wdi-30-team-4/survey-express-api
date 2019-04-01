f80e18215aeb3d3accba3f8f30eb7d7744e52c3d
pprakasam <padmaprakasam@gmail.com>
2019-04-01 13:47:06 -0400

    Restrict user to take survey only once
    Add logic to restrict a user to respond more than once.

--- app/routes/response_routes.js
+++ app/routes/response_routes.js
@@ -20,17 +20,38 @@ router.get('/responses', requireToken, (req, res, next) => {
 // POST /responses
 router.post('/responses', requireToken, (req, res, next) => {
   req.body.response.owner = req.user.id
+  // let resId = null
+  // Response.findOne({ owner: req.user.id }, {question: req.body.response.question})
+  //   .then((data) => {
+  //     if (data === null) {
+  //       Response.create(req.body.response)
+  //         .then(response => {
+  //           // set responseId for later use
+  //           resId = response.id
+  //           // return the Survey thats being responded to
+  //           return Survey.findById(req.body.response.question)
+  //         })
+  //         .then(survey => {
+  //           // push the current response to the survey
+  //           return survey.update({$push: {response: resId}})
+  //         })
+  //         .then(() => res.sendStatus(204))
+  //     } else {
+  //       res.status(500).json({ message: 'Some Error!' })
+  //     }
+  //   })
+  // .catch(next)
+  // })
   let resId = null
-
   Response.create(req.body.response)
     .then(response => {
-      // set responseId for later use
+    // set responseId for later use
       resId = response.id
       // return the Survey thats being responded to
       return Survey.findById(req.body.response.question)
     })
     .then(survey => {
-      // push the current response to the survey
+    // push the current response to the survey
       return survey.update({$push: {response: resId}})
     })
     .then(() => res.sendStatus(204))
